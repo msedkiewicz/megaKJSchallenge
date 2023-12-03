49e7e5d5dae4438d30d74303d6c81791092819b0
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-03 19:43:17 +0100

    Code refactoring

--- calculator.js
+++ calculator.js
@@ -9,41 +9,30 @@ do {
     var val1 = parseInt(prompt("Podaj pierwszą liczbę"));
     var val2 = parseInt(prompt("Podaj drugą liczbę"));
     var operation = prompt("Podaj działanie (+ - * /).");
+    var result;
 
     if (operation === "+") {
-      console.log(
-        val1 + " + " + val2 + " = " + (val1 + val2)
-      );
-      operationsHistory.push(
-        val1 + " + " + val2 + " = " + (val1 + val2)
-      );
+      result = val1 + " + " + val2 + " = " + (val1 + val2);
+      console.log(result);
+      operationsHistory.push(result);
     }
 
     if (operation === "-") {
-      console.log(
-        val1 + " - " + val2 + " = " + (val1 - val2)
-      );
-      operationsHistory.push(
-        val1 + " - " + val2 + " = " + (val1 - val2)
-      );
+      result = val1 + " - " + val2 + " = " + (val1 - val2);
+      console.log(result);
+      operationsHistory.push(result);
     }
 
     if (operation === "*") {
-      console.log(
-        val1 + " * " + val2 + " = " + (val1 * val2)
-      );
-      operationsHistory.push(
-        val1 + " * " + val2 + " = " + (val1 * val2)
-      );
+      result = val1 + " * " + val2 + " = " + val1 * val2;
+      console.log(result);
+      operationsHistory.push(result);
     }
 
     if (operation === "/") {
-      console.log(
-        val1 + " / " + val2 + " = " + (val1 / val2)
-      );
-      operationsHistory.push(
-        val1 + " / " + val2 + " = " + (val1 / val2)
-      );
+      result = val1 + " / " + val2 + " = " + val1 / val2;
+      console.log(result);
+      operationsHistory.push(result);
     }
   }
 } while (choice !== "N");
