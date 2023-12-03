3f24af5a0d8532b618a168ed7e1b9b881d94b3c1
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-03 21:20:26 +0100

    Add basic function structure

--- calculator.js
+++ calculator.js
@@ -1,38 +1,51 @@
-var operationsHistory = [];
+var val1 = parseInt(prompt("Podaj pierwszą liczbę"));
+var val2 = parseInt(prompt("Podaj drugą liczbę"));
+var operation = prompt("Podaj działanie (+ - * /).");
 
-do {
-  var val1 = parseInt(prompt("Podaj pierwszą liczbę"));
-  var val2 = parseInt(prompt("Podaj drugą liczbę"));
-  var operation = prompt("Podaj działanie (+ - * /).");
-  var result;
-  if (isNaN(val1) || isNaN(val2)) {
-    result =
-      "Przynajmniej jedna z wartości jest niepoprawna. Proszę wpisać liczbę";
-  } else {
-    if (operation === "+") {
-      result = val1 + " + " + val2 + " = " + (val1 + val2);
-    } else if (operation === "-") {
-      result = val1 + " - " + val2 + " = " + (val1 - val2);
-    } else if (operation === "*") {
-      result = val1 + " * " + val2 + " = " + val1 * val2;
-    } else if (operation === "/") {
-      if (val2 === 0) {
-        result = "Nie możesz dzielić przez zero!";
-      } else {
-        result = val1 + " / " + val2 + " = " + val1 / val2;
-      }
-    } else {
-      result =
-        "Zostało podane niepoprawne działanie. Podaj jedno z wybranych działań (+ - * /).";
+function calculate(a, b, sign) {
+    switch(sign) {
+        case '+': ;
+        case '-': ;
+        case '*': ;
+        case '/': ;
+        default: ;
     }
-  }
+}
+// var operationsHistory = [];
 
-  console.log(result);
-  operationsHistory.push(result);
+// do {
+//   var val1 = parseInt(prompt("Podaj pierwszą liczbę"));
+//   var val2 = parseInt(prompt("Podaj drugą liczbę"));
+//   var operation = prompt("Podaj działanie (+ - * /).");
+//   var result;
+//   if (isNaN(val1) || isNaN(val2)) {
+//     result =
+//       "Przynajmniej jedna z wartości jest niepoprawna. Proszę wpisać liczbę";
+//   } else {
+//     if (operation === "+") {
+//       result = val1 + " + " + val2 + " = " + (val1 + val2);
+//     } else if (operation === "-") {
+//       result = val1 + " - " + val2 + " = " + (val1 - val2);
+//     } else if (operation === "*") {
+//       result = val1 + " * " + val2 + " = " + val1 * val2;
+//     } else if (operation === "/") {
+//       if (val2 === 0) {
+//         result = "Nie możesz dzielić przez zero!";
+//       } else {
+//         result = val1 + " / " + val2 + " = " + val1 / val2;
+//       }
+//     } else {
+//       result =
+//         "Zostało podane niepoprawne działanie. Podaj jedno z wybranych działań (+ - * /).";
+//     }
+//   }
 
-  var choice = prompt(
-    "Czy chcesz obliczać dalej? Wpisz N aby zakończyć działanie."
-  );
-} while (choice !== "N");
+//   console.log(result);
+//   operationsHistory.push(result);
 
-console.log(operationsHistory);
+//   var choice = prompt(
+//     "Czy chcesz obliczać dalej? Wpisz N aby zakończyć działanie."
+//   );
+// } while (choice !== "N");
+
+// console.log(operationsHistory);
