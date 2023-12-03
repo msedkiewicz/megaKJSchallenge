56e3fb3b14c5137cb6558cbed0b2483298370ea5
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-03 21:15:56 +0100

    Code refactoring

--- calculator.js
+++ calculator.js
@@ -1,36 +1,35 @@
 var operationsHistory = [];
 
 do {
-  if (choice !== "N") {
-    var val1 = parseInt(prompt("Podaj pierwszą liczbę"));
-    var val2 = parseInt(prompt("Podaj drugą liczbę"));
-    var operation = prompt("Podaj działanie (+ - * /).");
-    var result;
-    if (isNaN(val1) || isNaN(val2)) {
-      result =
-        "Przynajmniej jedna z wartości jest niepoprawna. Proszę wpisać liczbę";
-    } else {
-      if (operation === "+") {
-        result = val1 + " + " + val2 + " = " + (val1 + val2);
-      } else if (operation === "-") {
-        result = val1 + " - " + val2 + " = " + (val1 - val2);
-      } else if (operation === "*") {
-        result = val1 + " * " + val2 + " = " + val1 * val2;
-      } else if (operation === "/") {
-        if (val2 === 0) {
-          result = "Nie możesz dzielić przez zero!";
-        } else {
-          result = val1 + " / " + val2 + " = " + val1 / val2;
-        }
+  var val1 = parseInt(prompt("Podaj pierwszą liczbę"));
+  var val2 = parseInt(prompt("Podaj drugą liczbę"));
+  var operation = prompt("Podaj działanie (+ - * /).");
+  var result;
+  if (isNaN(val1) || isNaN(val2)) {
+    result =
+      "Przynajmniej jedna z wartości jest niepoprawna. Proszę wpisać liczbę";
+  } else {
+    if (operation === "+") {
+      result = val1 + " + " + val2 + " = " + (val1 + val2);
+    } else if (operation === "-") {
+      result = val1 + " - " + val2 + " = " + (val1 - val2);
+    } else if (operation === "*") {
+      result = val1 + " * " + val2 + " = " + val1 * val2;
+    } else if (operation === "/") {
+      if (val2 === 0) {
+        result = "Nie możesz dzielić przez zero!";
       } else {
-        result =
-          "Zostało podane niepoprawne działanie. Podaj jedno z wybranych działań (+ - * /).";
+        result = val1 + " / " + val2 + " = " + val1 / val2;
       }
+    } else {
+      result =
+        "Zostało podane niepoprawne działanie. Podaj jedno z wybranych działań (+ - * /).";
     }
-
-    console.log(result);
-    operationsHistory.push(result);
   }
+
+  console.log(result);
+  operationsHistory.push(result);
+
   var choice = prompt(
     "Czy chcesz obliczać dalej? Wpisz N aby zakończyć działanie."
   );
