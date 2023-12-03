dbc4bd49aa48500f8b8687dfebdc6076abd85607
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-03 19:33:20 +0100

    Code formatting

--- calculator.js
+++ calculator.js
@@ -1,29 +1,43 @@
 var operationsHistory = [];
 
 do {
-var val1 = prompt('Podaj pierwszą liczbę');
-var val2 = prompt('Podaj drugą liczbę');
-var operation = prompt('Podaj działanie (+ - * /). Podaj N aby zakończyć działanie.');
+  var val1 = prompt("Podaj pierwszą liczbę");
+  var val2 = prompt("Podaj drugą liczbę");
+  var operation = prompt(
+    "Podaj działanie (+ - * /). Podaj N aby zakończyć działanie."
+  );
 
-if(operation === '+') {
-    console.log(val1 + ' + ' + val2 + ' = ' + (parseInt(val1) + parseInt(val2)));
-    operationsHistory.push(val1 + ' + ' + val2 + ' = ' + (parseInt(val1) + parseInt(val2)));
-}
+  if (operation === "+") {
+    console.log(
+      val1 + " + " + val2 + " = " + (parseInt(val1) + parseInt(val2))
+    );
+    operationsHistory.push(
+      val1 + " + " + val2 + " = " + (parseInt(val1) + parseInt(val2))
+    );
+  }
 
-if(operation === '-') {
-    console.log(val1 + ' - ' + val2 + ' = ' + (parseInt(val1) - parseInt(val2)));
-    operationsHistory.push(val1 + ' - ' + val2 + ' = ' + (parseInt(val1) - parseInt(val2)));
-}
+  if (operation === "-") {
+    console.log(
+      val1 + " - " + val2 + " = " + (parseInt(val1) - parseInt(val2))
+    );
+    operationsHistory.push(
+      val1 + " - " + val2 + " = " + (parseInt(val1) - parseInt(val2))
+    );
+  }
 
-if(operation === '*') {
-    console.log(val1 + ' * ' + val2 + ' = ' + (parseInt(val1) * parseInt(val2)));
-    operationsHistory.push(val1 + ' * ' + val2 + ' = ' + (parseInt(val1) * parseInt(val2)));
-}
+  if (operation === "*") {
+    console.log(val1 + " * " + val2 + " = " + parseInt(val1) * parseInt(val2));
+    operationsHistory.push(
+      val1 + " * " + val2 + " = " + parseInt(val1) * parseInt(val2)
+    );
+  }
 
-if(operation === '/') {
-    console.log(val1 + ' / ' + val2 + ' = ' + (parseInt(val1) / parseInt(val2)));
-    operationsHistory.push(val1 + ' / ' + val2 + ' = ' + (parseInt(val1) / parseInt(val2)));
-}
-} while(operation !== 'N');
+  if (operation === "/") {
+    console.log(val1 + " / " + val2 + " = " + parseInt(val1) / parseInt(val2));
+    operationsHistory.push(
+      val1 + " / " + val2 + " = " + parseInt(val1) / parseInt(val2)
+    );
+  }
+} while (operation !== "N");
 
 console.log(operationsHistory);
