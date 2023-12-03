54d73333ca24188a1d59d4a4eba8e1390011e70c
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-03 19:32:24 +0100

    Add option for more operations

--- calculator.js
+++ calculator.js
@@ -1,7 +1,9 @@
+var operationsHistory = [];
+
+do {
 var val1 = prompt('Podaj pierwszą liczbę');
 var val2 = prompt('Podaj drugą liczbę');
-var operation = prompt('Podaj działanie (+ - * /)');
-var operationsHistory = [];
+var operation = prompt('Podaj działanie (+ - * /). Podaj N aby zakończyć działanie.');
 
 if(operation === '+') {
     console.log(val1 + ' + ' + val2 + ' = ' + (parseInt(val1) + parseInt(val2)));
@@ -22,5 +24,6 @@ if(operation === '/') {
     console.log(val1 + ' / ' + val2 + ' = ' + (parseInt(val1) / parseInt(val2)));
     operationsHistory.push(val1 + ' / ' + val2 + ' = ' + (parseInt(val1) / parseInt(val2)));
 }
+} while(operation !== 'N');
 
 console.log(operationsHistory);
