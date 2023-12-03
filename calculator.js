a51969a75f710dd77ab146a67f03a73bb7842277
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-03 19:36:39 +0100

    Change exit mode placement

--- calculator.js
+++ calculator.js
@@ -1,43 +1,51 @@
 var operationsHistory = [];
 
 do {
-  var val1 = prompt("Podaj pierwszą liczbę");
-  var val2 = prompt("Podaj drugą liczbę");
-  var operation = prompt(
-    "Podaj działanie (+ - * /). Podaj N aby zakończyć działanie."
+  var choice = prompt(
+    "Czy chcesz obliczać dalej? Wpisz N aby zakończyć działanie."
   );
 
-  if (operation === "+") {
-    console.log(
-      val1 + " + " + val2 + " = " + (parseInt(val1) + parseInt(val2))
-    );
-    operationsHistory.push(
-      val1 + " + " + val2 + " = " + (parseInt(val1) + parseInt(val2))
-    );
-  }
+  if (choice !== "N") {
+    var val1 = prompt("Podaj pierwszą liczbę");
+    var val2 = prompt("Podaj drugą liczbę");
+    var operation = prompt("Podaj działanie (+ - * /).");
 
-  if (operation === "-") {
-    console.log(
-      val1 + " - " + val2 + " = " + (parseInt(val1) - parseInt(val2))
-    );
-    operationsHistory.push(
-      val1 + " - " + val2 + " = " + (parseInt(val1) - parseInt(val2))
-    );
-  }
+    if (operation === "+") {
+      console.log(
+        val1 + " + " + val2 + " = " + (parseInt(val1) + parseInt(val2))
+      );
+      operationsHistory.push(
+        val1 + " + " + val2 + " = " + (parseInt(val1) + parseInt(val2))
+      );
+    }
 
-  if (operation === "*") {
-    console.log(val1 + " * " + val2 + " = " + parseInt(val1) * parseInt(val2));
-    operationsHistory.push(
-      val1 + " * " + val2 + " = " + parseInt(val1) * parseInt(val2)
-    );
-  }
+    if (operation === "-") {
+      console.log(
+        val1 + " - " + val2 + " = " + (parseInt(val1) - parseInt(val2))
+      );
+      operationsHistory.push(
+        val1 + " - " + val2 + " = " + (parseInt(val1) - parseInt(val2))
+      );
+    }
 
-  if (operation === "/") {
-    console.log(val1 + " / " + val2 + " = " + parseInt(val1) / parseInt(val2));
-    operationsHistory.push(
-      val1 + " / " + val2 + " = " + parseInt(val1) / parseInt(val2)
-    );
+    if (operation === "*") {
+      console.log(
+        val1 + " * " + val2 + " = " + parseInt(val1) * parseInt(val2)
+      );
+      operationsHistory.push(
+        val1 + " * " + val2 + " = " + parseInt(val1) * parseInt(val2)
+      );
+    }
+
+    if (operation === "/") {
+      console.log(
+        val1 + " / " + val2 + " = " + parseInt(val1) / parseInt(val2)
+      );
+      operationsHistory.push(
+        val1 + " / " + val2 + " = " + parseInt(val1) / parseInt(val2)
+      );
+    }
   }
-} while (operation !== "N");
+} while (choice !== "N");
 
 console.log(operationsHistory);
