c7c410630b3cdd81b2251e38dede286b05a014a4
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-03 19:26:20 +0100

    Add basic operation history code

--- calculator.js
+++ calculator.js
@@ -1,19 +1,26 @@
 var val1 = prompt('Podaj pierwszą liczbę');
 var val2 = prompt('Podaj drugą liczbę');
 var operation = prompt('Podaj działanie (+ - * /)');
+var operationsHistory = [];
 
 if(operation === '+') {
-    console.log(val1 + ' + ' + val2 + ' = ' + (parseInt(val1) + parseInt(val2)))
+    console.log(val1 + ' + ' + val2 + ' = ' + (parseInt(val1) + parseInt(val2)));
+    operationsHistory.push(val1 + ' + ' + val2 + ' = ' + (parseInt(val1) + parseInt(val2)));
 }
 
 if(operation === '-') {
-    console.log(val1 + ' - ' + val2 + ' = ' + (parseInt(val1) - parseInt(val2)))
+    console.log(val1 + ' - ' + val2 + ' = ' + (parseInt(val1) - parseInt(val2)));
+    operationsHistory.push(val1 + ' - ' + val2 + ' = ' + (parseInt(val1) - parseInt(val2)));
 }
 
 if(operation === '*') {
-    console.log(val1 + ' * ' + val2 + ' = ' + (parseInt(val1) * parseInt(val2)))
+    console.log(val1 + ' * ' + val2 + ' = ' + (parseInt(val1) * parseInt(val2)));
+    operationsHistory.push(val1 + ' * ' + val2 + ' = ' + (parseInt(val1) * parseInt(val2)));
 }
 
 if(operation === '/') {
-    console.log(val1 + ' / ' + val2 + ' = ' + (parseInt(val1) / parseInt(val2)))
+    console.log(val1 + ' / ' + val2 + ' = ' + (parseInt(val1) / parseInt(val2)));
+    operationsHistory.push(val1 + ' / ' + val2 + ' = ' + (parseInt(val1) / parseInt(val2)));
 }
+
+console.log(operationsHistory);
