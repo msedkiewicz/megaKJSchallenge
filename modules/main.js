89d649fafa7b14c340b36f7d05cba95feea4a88a
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-07 21:53:43 +0100

    Import / export modules part 2

--- modules/main.js
+++ modules/main.js
@@ -1,9 +1,9 @@
-import Square from "./square.js";
+import LoremIpsum from "./square.js"; // not recommended (using different name for import)
 
 const squares = [];
 
 for (let i = 0; i < 10; i++) {
-    const newShape = new Square(Math.round(Math.random() * 50 + 5));
+    const newShape = new LoremIpsum(Math.round(Math.random() * 50 + 5));
     squares.push(newShape);
 }
 
