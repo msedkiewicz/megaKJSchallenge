51dd9970f8b0e7e2941cae37dc381e228c378337
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-07 21:57:01 +0100

    Import / export modules part 4 - using alias

--- modules/main.js
+++ modules/main.js
@@ -1,9 +1,9 @@
-import { Square } from "./square.js"; // not recommended (using different name for import)
+import { Square as SuperSquare } from "./square.js"; // not recommended (using different name for import)
 
 const squares = [];
 
 for (let i = 0; i < 10; i++) {
-    const newShape = new Square(Math.round(Math.random() * 50 + 5));
+    const newShape = new SuperSquare(Math.round(Math.random() * 50 + 5));
     squares.push(newShape);
 }
 
