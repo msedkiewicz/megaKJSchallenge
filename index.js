f7cd0bb3fa8a2d35bc8d98c2b213ca54ecef658a
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-07 13:55:31 +0100

    Add name changing

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ class User {
     sayHello() {
         console.log(`Hello, my name is ${this.name}`);
     }
+
+    changeName(newName) {
+        this.name = newName;
+    }
 }
 
 const marcin = new User('Marcin', 'Grygierek');
 const marian = new User('Marian', 'Nowak');
 
 marcin.sayHello();
+marian.sayHello();
+
+marian.changeName('Łukasz');
 marian.sayHello();
