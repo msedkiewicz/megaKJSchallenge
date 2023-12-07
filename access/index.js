9a78ef09d4840ab0ca56fee54b4e1cea731206bd
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-07 21:29:20 +0100

    Readability part 3 - using hash (private)

--- access/index.js
+++ access/index.js
@@ -1,24 +1,31 @@
 class User {
-    _hash = '';
+    #hash = '';
     constructor(name) {
         this.name = name;
-        this.hash = (Math.random() * 100000000 + 100000000).toString();
+        this.#hash = (Math.random() * 100000000 + 100000000).toString();
     }
 
     set  hash(newHash) {
         if(newHash.length < 4) return;
-        this._hash = newHash;
+        this.#hash = newHash;
     }
 
     get hash() {
-        return this._hash;
+        return this.#getHash();
+    }
+
+    #getHash() {
+        return this.#hash;
     }
 }
 
 const user = new User('Marcin');
-// console.log(user.hash('askksks')); - hash is not a fuction :P
 
 user.hash = 'Nowa nadzieja';
 console.log(user.hash);
 user.hash = 'Nowa nadzieja 2';
 console.log(user.hash);
+// console.log(user.#hash); // Private field '#hash' must be declared in an enclosing class
+user.hash = 'Nowa nadzieja 3 i 3/4';
+console.log(user.hash);
+// console.log(user.#getHash); // Private field '#getHash' must be declared in an enclosing class
