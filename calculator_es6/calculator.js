5d8396aad511bc1d7c1288ef993660bac892012b
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-11 15:03:38 +0100

    Add decimal calculator; add console.logs in abstract class

--- calculator_es6/calculator.js
+++ calculator_es6/calculator.js
@@ -1,67 +1,80 @@
 class Calculator {
   // abstract
 
-  #operationHistory = [];
+  run() {
+    console.log("NOT IMPLEMENTED, ANY CLASS MUST IMPLEMENT run()");
+  }
 
-  run() {}
+  calculate(a, b, sign) {
+    console.log(
+      "NOT IMPLEMENTED, ANY CLASS MUST IMPLEMENT calculate(a, b, sign)"
+    );
+  }
 
-  calculate(a, b, sign) {}
+  addToHistory(operation) {
+    // throw new Error( // wyjątki - dla metod niezbędnych
+    //   "NOT IMPLEMENTED, ANY CLASS MUST IMPLEMENT addToHistory(operation)"
+    // );
+    console.log(
+      "NOT IMPLEMENTED, ANY CLASS MUST IMPLEMENT addToHistory(operation)"
+    );
+  }
 
-  addToHistory(operation) {}
-
-  printHistory() {}
+  printHistory() {
+    console.log("NOT IMPLEMENTED, ANY CLASS MUST IMPLEMENT printHistory()");
+  }
 }
 
-// class Calculator {
-//   #operationHistory = [];
+class DecimalCalculator extends Calculator {
+  #operationHistory = [];
 
-//   calculate(a, b, sign) {
-//     if (isNaN(a) || isNaN(b))
-//       return "Przynajmniej jedna z wartości jest niepoprawna. Proszę wpisać liczbę";
+  calculate(a, b, sign) {
+    if (isNaN(a) || isNaN(b))
+      return "Przynajmniej jedna z wartości jest niepoprawna. Proszę wpisać liczbę";
 
-//     switch (sign) {
-//       case "+":
-//         return a + " + " + b + " = " + (a + b);
-//       case "-":
-//         return a + " - " + b + " = " + (a - b);
-//       case "*":
-//         return a + " * " + b + " = " + a * b;
-//       case "/": {
-//         if (b === 0) return "Nie możesz dzielić przez zero!";
-//         return a + " / " + b + " = " + a / b;
-//       }
+    switch (sign) {
+      case "+":
+        return a + " + " + b + " = " + (a + b);
+      case "-":
+        return a + " - " + b + " = " + (a - b);
+      case "*":
+        return a + " * " + b + " = " + a * b;
+      case "/": {
+        if (b === 0) return "Nie możesz dzielić przez zero!";
+        return a + " / " + b + " = " + a / b;
+      }
 
-//       default:
-//         return "Zostało podane niepoprawne działanie. Podaj jedno z wybranych działań (+ - * /).";
-//     }
-//   }
+      default:
+        return "Zostało podane niepoprawne działanie. Podaj jedno z wybranych działań (+ - * /).";
+    }
+  }
 
-//   addToHistory(operation) {
-//     this.#operationHistory.push(operation);
-//   }
+  addToHistory(operation) {
+    this.#operationHistory.push(operation);
+  }
 
-//   printHistory() {
-//     this.#operationHistory.forEach((operation, index) => {
-//       console.log(`Operacja nr ${index + 1}: ${operation}`);
-//     });
-//   }
+  printHistory() {
+    this.#operationHistory.forEach((operation, index) => {
+      console.log(`Operacja nr ${index + 1}: ${operation}`);
+    });
+  }
 
-//   run() {
-//     let choice;
-//     do {
-//       const val1 = parseInt(prompt("Podaj pierwszą liczbę"));
-//       const val2 = parseInt(prompt("Podaj drugą liczbę"));
-//       const operation = prompt("Podaj działanie (+ - * /).");
+  run() {
+    let choice;
+    do {
+      const val1 = parseInt(prompt("Podaj pierwszą liczbę"));
+      const val2 = parseInt(prompt("Podaj drugą liczbę"));
+      const operation = prompt("Podaj działanie (+ - * /).");
 
-//       const result = this.calculate(val1, val2, operation);
-//       this.addToHistory(result);
-//       choice = prompt(
-//         "Czy chcesz obliczać dalej? Wpisz N aby zakończyć działanie."
-//       );
-//     } while (choice !== "N");
-//     this.printHistory();
-//   }
-// }
+      const result = this.calculate(val1, val2, operation);
+      this.addToHistory(result);
+      choice = prompt(
+        "Czy chcesz obliczać dalej? Wpisz N aby zakończyć działanie."
+      );
+    } while (choice !== "N");
+    this.printHistory();
+  }
+}
 
-// const calculator = new Calculator();
-// calculator.run();
+const decimalCalculator = new DecimalCalculator();
+decimalCalculator.run();
