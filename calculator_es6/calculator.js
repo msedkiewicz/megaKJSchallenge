bcba70d5f56a1e5c4a461af9741e20d38b993af2
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-11 14:03:47 +0100

    Add calculate method code

--- calculator_es6/calculator.js
+++ calculator_es6/calculator.js
@@ -1,7 +1,26 @@
 class Calculator {
   operationHistory = [];
 
-  calculate() {}
+  calculate() {
+    if (isNaN(a) || isNaN(b))
+      return "Przynajmniej jedna z wartości jest niepoprawna. Proszę wpisać liczbę";
+
+    switch (sign) {
+      case "+":
+        return a + " + " + b + " = " + (a + b);
+      case "-":
+        return a + " - " + b + " = " + (a - b);
+      case "*":
+        return a + " * " + b + " = " + a * b;
+      case "/": {
+        if (b === 0) return "Nie możesz dzielić przez zero!";
+        return a + " / " + b + " = " + a / b;
+      }
+
+      default:
+        return "Zostało podane niepoprawne działanie. Podaj jedno z wybranych działań (+ - * /).";
+    }
+  }
 
   addToHisotry() {}
 
@@ -10,6 +29,9 @@ class Calculator {
   run() {
     let choice;
     do {
+      const val1 = parseInt(prompt("Podaj pierwszą liczbę"));
+      const val2 = parseInt(prompt("Podaj drugą liczbę"));
+      const operation = prompt("Podaj działanie (+ - * /).");
       console.log("Działam!");
       choice = prompt(
         "Czy chcesz obliczać dalej? Wpisz N aby zakończyć działanie."
