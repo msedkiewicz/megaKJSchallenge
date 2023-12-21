f230d43f4149bc8b8885581fcc7935db46e48812
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-21 13:35:41 +0100

    Add counter

--- classes-notes/algorhitms/bubble_sort.js
+++ classes-notes/algorhitms/bubble_sort.js
@@ -1,7 +1,9 @@
-const arr = [5, 2, 3, 1, 4];
+const arr = [15, 2, 3, 21, 4, 6];
 
+counter = 0;
 for (let j = 0; j < arr.length; j++) {
   for (let i = 0; i < arr.length - 1; i++) {
+    counter++;
     if (arr[i] > arr[i + 1]) {
       const temp = arr[i];
       arr[i] = arr[i + 1];
@@ -11,3 +13,4 @@ for (let j = 0; j < arr.length; j++) {
 }
 
 console.log(arr);
+console.log(arr.length, counter);
