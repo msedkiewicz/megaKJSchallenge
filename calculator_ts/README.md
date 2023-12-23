cbc182ca04b4e22996672e06a9418fba2369874b
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-23 13:47:42 +0100

    Update Readme and TS calculator

--- calculator_ts/README.md
+++ calculator_ts/README.md
@@ -2,24 +2,18 @@
 
 This repository contains basic, TypeScript based calculator.
 
-<!-- Calculator instructions are in Polish. Calculator allows to add, subtract, multiply and divide. Structure is based on alert function. Final version is written in OOP approach. -->
-
 ## For developers
 
 If you want to develop code from this repo, you need to use TS compiler. For VSC you can find instructions here: https://code.visualstudio.com/docs/languages/typescript.
 
-Currently this calculator requires additional TS configuration.
-
-<!-- download it and open index.html file in a browser. JS code is in attached JS file. -->
+Currently this calculator requires additional TS configuration. To work with it I recomment using TS Playground: https://www.typescriptlang.org/play.
 
 # Kalkulator ES6
 
 To repozytorium zawiera podstawowy kalkulator napisany w TypeScript.
 
-<!-- Instrukcje kalkulatora są w języku polskim. Kalkulator umożliwia dodawanie, odejmowanie, mnożenie i dzielenie. Struktura opiera się na funkcji alert. Ostateczna wersja jest napisana w podejściu OOP. -->
-
 ## Dla developerów
 
-Aby korzystać z tego kodu przydatny będzie kompilator dla TS, np. https://code.visualstudio.com/docs/languages/typescript. Obecnie repozytorium wymaga dodatkowej konfiguracji dla TS.
+Aby korzystać z tego kodu przydatny będzie kompilator dla TS, np. https://code.visualstudio.com/docs/languages/typescript.
 
-<!-- Jeśli chcesz rozwijać kod z tego repozytorium, wystarczy go pobrać i otworzyć plik index.html w przeglądarce. Kod JS znajduje się w załączonym pliku JS. -->
+Obecnie repozytorium wymaga dodatkowej konfiguracji dla TS. Dlatego w pracy z tym repo rekomenduję wykorzystanie playgrounda dla TS: https://www.typescriptlang.org/play.
--- calculator_ts/calculator.js
+++ calculator_ts/calculator.js
@@ -1,117 +1,100 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var OperationsMap = /** @class */ (function (_super) {
-    __extends(OperationsMap, _super);
-    function OperationsMap() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    return OperationsMap;
-}(Map));
+"use strict";
+class OperationsMap extends Map {}
 var Arithmetic;
 (function (Arithmetic) {
-    Arithmetic["Add"] = "+";
-    Arithmetic["Substract"] = "-";
-    Arithmetic["Multiply"] = "*";
-    Arithmetic["Divide"] = "/";
-    Arithmetic["Power"] = "^";
+  Arithmetic["Add"] = "+";
+  Arithmetic["Substract"] = "-";
+  Arithmetic["Multiply"] = "*";
+  Arithmetic["Divide"] = "/";
+  Arithmetic["Power"] = "^";
 })(Arithmetic || (Arithmetic = {}));
-var Operation = /** @class */ (function () {
-    function Operation(callback) {
-        this.callback = callback;
-    }
-    Operation.prototype.perform = function (a, b) {
-        return this.callback(a, b);
-    };
-    return Operation;
-}());
-var Calculator = /** @class */ (function () {
-    function Calculator(name, description, operations, allowedOperations) {
-        this.name = name;
-        this.description = description;
-        this.operations = operations;
-        this.allowedOperations = allowedOperations;
-    }
-    Calculator.prototype.getSign = function (operation) {
-        return this.allowedOperations.find(function (sign) { return operation.includes(sign); });
-    };
-    Calculator.prototype.getValues = function (operation, sign) {
-        var values = operation.split(sign).map(function (val) { return parseInt(val); });
-        return values;
-    };
-    Calculator.prototype.getInfo = function () {
-        return "".concat(this.name, " : ").concat(this.description);
-    };
-    Calculator.prototype.calculate = function (operation) {
-        var sign = this.getSign(operation);
-        if (!sign || !this.allowedOperations.includes(sign))
-            return "Niedozwolona operacja. Użyj znaku +, -, *, /.";
-        var values = this.getValues(operation, sign);
-        if (values.length !== 2)
-            return 'Niepoprawna operacja!';
-        var method = this.operations.get(sign);
-        if (!method)
-            return "Ta operacja nie jest jeszcze możliwa";
-        return method.perform(values[0], values[1]);
-    };
-    return Calculator;
-}());
-var CalculatorBuilder = /** @class */ (function () {
-    function CalculatorBuilder() {
-        this.operations = new OperationsMap();
-        this.allowedOperations = [];
-        this.name = '';
-        this.description = '';
-    }
-    CalculatorBuilder.prototype.addOperation = function (sign, callback) {
-        var operation = new Operation(callback);
-        this.operations.set(sign, operation);
-        return this;
-    };
-    CalculatorBuilder.prototype.setName = function (name) {
-        this.name = name;
-        return this;
-    };
-    CalculatorBuilder.prototype.setDescription = function (description) {
-        this.description = description;
-        return this;
-    };
-    CalculatorBuilder.prototype.setAllowedOperations = function () {
-        var operations = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            operations[_i] = arguments[_i];
-        }
-        this.allowedOperations = operations;
-        return this;
-    };
-    CalculatorBuilder.prototype.build = function () {
-        var calculator = new Calculator(this.name, this.description, this.operations, this.allowedOperations);
-        return calculator;
-    };
-    return CalculatorBuilder;
-}());
-var calculator = new CalculatorBuilder()
-    .setName("Kalkulator w systemie dziesiątkowym")
-    .setDescription("Prosty kalkulator obliczający sumę, różnicę, iloczyn i iloraz w systemie dziesiątkowym")
-    .setAllowedOperations(Arithmetic.Add, Arithmetic.Substract, Arithmetic.Multiply, Arithmetic.Divide, Arithmetic.Power)
-    .addOperation(Arithmetic.Add, function (a, b) { return a + b; })
-    .addOperation(Arithmetic.Substract, function (a, b) { return a - b; })
-    .addOperation(Arithmetic.Multiply, function (a, b) { return a * b; })
-    .addOperation(Arithmetic.Divide, function (a, b) {
-    if (b === 0)
-        throw new Error("Nie możesz dzielić przez 0!");
+class Operation {
+  constructor(callback) {
+    this.callback = callback;
+  }
+  perform(a, b) {
+    return this.callback(a, b);
+  }
+}
+class Calculator {
+  constructor(name, description, operations, allowedOperations) {
+    this.name = name;
+    this.description = description;
+    this.operations = operations;
+    this.allowedOperations = allowedOperations;
+  }
+  getSign(operation) {
+    return this.allowedOperations.find((sign) => operation.includes(sign));
+  }
+  getValues(operation, sign) {
+    const values = operation.split(sign).map((val) => parseInt(val));
+    return values;
+  }
+  getInfo() {
+    return `${this.name} : ${this.description}`;
+  }
+  calculate(operation) {
+    const sign = this.getSign(operation);
+    if (!sign || !this.allowedOperations.includes(sign))
+      return "Niedozwolona operacja. Użyj znaku +, -, *, /.";
+    const values = this.getValues(operation, sign);
+    if (values.length !== 2) return "Niepoprawna operacja!";
+    const method = this.operations.get(sign);
+    if (!method) return "Ta operacja nie jest jeszcze możliwa";
+    return method.perform(values[0], values[1]);
+  }
+}
+class CalculatorBuilder {
+  constructor() {
+    this.operations = new OperationsMap();
+    this.allowedOperations = [];
+    this.name = "";
+    this.description = "";
+  }
+  addOperation(sign, callback) {
+    const operation = new Operation(callback);
+    this.operations.set(sign, operation);
+    return this;
+  }
+  setName(name) {
+    this.name = name;
+    return this;
+  }
+  setDescription(description) {
+    this.description = description;
+    return this;
+  }
+  setAllowedOperations(...operations) {
+    this.allowedOperations = operations;
+    return this;
+  }
+  build() {
+    const calculator = new Calculator(
+      this.name,
+      this.description,
+      this.operations,
+      this.allowedOperations
+    );
+    return calculator;
+  }
+}
+const calculator = new CalculatorBuilder()
+  .setName("Kalkulator w systemie dziesiątkowym")
+  .setDescription(
+    "Prosty kalkulator obliczający sumę, różnicę, iloczyn i iloraz w systemie dziesiątkowym"
+  )
+  .setAllowedOperations(
+    Arithmetic.Add,
+    Arithmetic.Substract,
+    Arithmetic.Multiply,
+    Arithmetic.Divide,
+    Arithmetic.Power
+  )
+  .addOperation(Arithmetic.Add, (a, b) => a + b)
+  .addOperation(Arithmetic.Substract, (a, b) => a - b)
+  .addOperation(Arithmetic.Multiply, (a, b) => a * b)
+  .addOperation(Arithmetic.Divide, (a, b) => {
+    if (b === 0) throw new Error("Nie możesz dzielić przez 0!");
     return a / b;
-})
-    .build();
+  })
+  .build();
